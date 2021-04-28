21432425a4b5af4d07d99dc13e8b0152d66470d3
bear-wolf <bear-wolf@ukr.net>
2021-04-28 15:23:52 +0300

    add template

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//import Vue from "vue";
+// import './assets/styles/styles.scss'
+
+
+// const App = new Vue({
+//     data: {
+//         title: 'Vue JS Template!'
+//     },
+//     store: {},
+//     el: '#app'
+// })
